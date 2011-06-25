99012e4162353d67caa5cc0b6def873e8f5f4a6d
JaanusSiim <jaanussiim@gmail.com>
2011-06-25 14:16:32 +0300

    added robolectric

---
+++ test/src/com/jaanussiim/slimtimer/android/activities/SlimtimerActivityTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2011 JaanusSiim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jaanussiim.slimtimer.android.activities;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricTestRunner.class)
+public class SlimtimerActivityTest {
+  private SlimtimerActivity slimtimerActivity;
+
+  @Before
+  public void setUp() {
+    slimtimerActivity = new SlimtimerActivity();
+  }
+
+  @Test
+  public void thisWillBeTrue() {
+    assertTrue(true);
+  }
+}
